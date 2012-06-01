93aab61ca03467df0b19eabeced9d0b46318aa10
Gordon Tyler <gordon@doxxx.net>
2012-06-01 14:01:02 -0400

    Fixed source paths in lein project.

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject net.doxxx/rift-combat-parser "1.0.0-SNAPSHOT"
   :description "Rift Combat Parser"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :source-path "src/main/clojure"
+  :test-path "src/test/clojure")
